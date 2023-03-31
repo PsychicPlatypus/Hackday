be6bd14de55443729c505adc66a182195e638a0f
Dzenis Madzovic <dzenis.madzovic@pinmeto.com>
2023-03-31 13:43:21 +0200

    Added test for bottom

--- src/gameLoop/test.ts
+++ src/gameLoop/test.ts
@@ -1,6 +1,7 @@
 import Field from "../field";
 import GameLoop from "./index";
 import { createSquare } from "../figureFactory";
+import Figure from "../figure";
 
 it("should be able to create square figure", () => {
     const field = new Field(5, 20);
@@ -17,4 +18,16 @@ it("should move the figure downwards after a single step", () => {
     gameLoop.figure = figure;
     gameLoop.step();
     expect(gameLoop.figure.coordinates).not.toEqual(currentFigureCoordinates);
+});
+
+it("should set the figure when it reaches bottom", () => {
+    const field = new Field(5, 20);
+    const gameLoop = new GameLoop(field);
+    const figure = new Figure([{ x: 0, y: 0 }], field);
+    const currentField = gameLoop.field;
+
+    gameLoop.figure = figure;
+    gameLoop.step();
+    expect(gameLoop.figure).toEqual(null);
+    expect(currentField.state.map).not.toEqual(gameLoop.field.state.map); // Field was changed
 });
