9e150ec0e93d9549db8fccbc5bdd389cb190952d
Dzenis Madzovic <dzenis.madzovic@pinmeto.com>
2023-03-31 13:30:26 +0200

    proper instance variables

--- src/gameLoop/test.ts
+++ src/gameLoop/test.ts
@@ -1,6 +1,8 @@
+import Field from "../field";
 import GameLoop from "./index";
 
 it("should be able to create square figure", () => {
-    const gameLoop = new GameLoop();
+    const field = new Field(5, 20);
+    const gameLoop = new GameLoop(field);
     expect(gameLoop).toBeDefined();
 });
